0218187f9aa2c4c7e531be4bdcc973e99c13d720
ghazal <146888669+ghazalkhb@users.noreply.github.com>
2024-02-09 00:17:54 -0500

    Update Similarity.py

--- Similarity.py
+++ Similarity.py
@@ -7,7 +7,7 @@ from node2vec import Node2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Set the path to the folder containing CSV files
-folder_path = '/home/morteza/Desktop/Alireza/Dataset/data/CallGraph/data/service/'
+folder_path = '[folder path]'
 
 # Get a list of all CSV files in the folder
 csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
@@ -18,13 +18,8 @@ graph_embeddings = {}
 
 # Loop through each CSV file
 for csv_file in csv_files:
-    # Construct the full path to the CSV file
     csv_file_path = os.path.join(folder_path, csv_file)
-
-    # Load the CSV file into a Pandas DataFrame
     df = pd.read_csv(csv_file_path)
-
-    # Create a graph from the DataFrame
     G = nx.from_pandas_edgelist(df, 'source', 'target')
 
     # Store the graph, its nodes, and embeddings in the dictionary
@@ -40,13 +35,6 @@ for csv_file in csv_files:
     node_embeddings = {node: model.wv[node] for node in G.nodes}
     graph_embeddings[csv_file] = node_embeddings
 
-# Define similarity range and initialize count dictionary
-similarity_ranges = np.arange(-1.0, 1.01, 0.01)
-count_per_range = {r: 0 for r in similarity_ranges}
-
-# Initialize count dictionary for each similarity range
-count_per_range = {r: 0 for r in similarity_ranges}
-
 # Calculate similarities between all pairs of graphs
 similarities = []
 
@@ -58,10 +46,6 @@ for i, (graph1, emb1) in enumerate(graph_embeddings.items()):
         similarity = cosine_similarity([avg_emb1], [avg_emb2])[0][0]
         similarities.append(similarity)
 
-        # Update counts for each similarity range
-        for r in similarity_ranges:
-            if similarity >= r:
-                count_per_range[r] += 1
 
 # Plot the histogram
 plt.bar(count_per_range.keys(), count_per_range.values(), width=0.02)
@@ -71,7 +55,7 @@ plt.title('Similarity Distribution')
 plt.show()
     
 # Sort similarities and print the most similar graph pairs
-k = 5  # Replace 5 with the desired number of most similar graph pairs to display
+k = 5  
 sorted_similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
 
 print("Lowest similarities:")
