121c7288dff6295625fa3d19eaf56b15a983c060
ghazal <146888669+ghazalkhb@users.noreply.github.com>
2024-02-10 15:07:32 -0500

    Update and rename similarity.py to Similarity.py

--- Similarity.py
+++ Similarity.py
@@ -8,8 +8,7 @@ from sklearn.metrics import silhouette_score
 
 
 # Set the path to the folder containing CSV files
-folder_path = '/home/morteza/Desktop/Alireza/Dataset/data/CallGraph/data/service1/'
-
+folder_path = '[folder Path]'
 # Get a list of all CSV files in the folder
 csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
@@ -18,27 +17,21 @@ graph_nodes = {}
 
 # Loop through each CSV file
 for csv_file in csv_files:
-    # Construct the full path to the CSV file
     csv_file_path = os.path.join(folder_path, csv_file)
-
-    # Load the CSV file into a Pandas DataFrame
     df = pd.read_csv(csv_file_path)
 
     # Create a graph from the DataFrame
     G = nx.from_pandas_edgelist(df, 'source', 'target')
-
-    # Store the graph and its nodes in the dictionary
     graph_nodes[csv_file] = list(G.nodes)
 
 # Extract features or similarity measures for clustering
-# Example: You can use the number of nodes and edges as features
 graph_features = {graph: (len(nodes), G.number_of_edges()) for graph, nodes in graph_nodes.items()}
 
 # Convert features to a Pandas DataFrame
 features_df = pd.DataFrame.from_dict(graph_features, orient='index', columns=['num_nodes', 'num_edges'])
 
 # Apply K-means clustering
-num_clusters = 6
+num_clusters = 5
 kmeans = KMeans(n_clusters=num_clusters, random_state=42)
 clusters = kmeans.fit_predict(features_df)
 
